abb6f2db2d5e9cb791bcf7fe8814ff9f7d6a1dce
Manak Upadhyay <manu742000@gmail.com>
2020-06-10 07:08:04 +0530

    minor change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
-    res.render('index',{});
+    res.render('index');
 });
 
 app.get('/read', function(req,res){
